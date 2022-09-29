0ba39e28e1fe7e2e98abe5c2b1b0893de466e7a4
longf <longf@DESKTOP-2GJ1G8J>
2022-09-29 20:41:26 +0200

    Add project files.

---
+++ Uppgift 3.17/Program.cs
@@ -0,0 +1,42 @@
+﻿using System; 
+namespace Upggift_3_17
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Skriv in ett tal");
+            string x=Console.ReadLine();
+           float ö=float.Parse(x);
+            Console.WriteLine("Skriv in ett tal till");
+            string y=Console.ReadLine();
+           float ä=float.Parse(y);
+            Console.WriteLine("Välj ett räknesätt");
+            Console.WriteLine("1. addition");
+            Console.WriteLine("2. Subtraktion");
+            Console.WriteLine("3. Multiplikation");
+            Console.WriteLine("4. Division");
+            string a=Console.ReadLine();
+            switch (a)
+            {
+                case "1":
+                    float b=ö + ä;
+                    Console.WriteLine(b);
+                    break;
+                    case "2":
+                    float c = ö - ä;
+                    Console.WriteLine(c);
+                    break;
+                case "3":
+                    float d = ö * ä;
+                    Console.WriteLine(d);
+                    break;
+                case "4":
+                    float e = ö / ä;
+                    Console.WriteLine(e);
+                    break;
+            }
+        }
+    }
+}
+
